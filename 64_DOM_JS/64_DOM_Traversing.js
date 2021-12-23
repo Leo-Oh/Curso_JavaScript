8d4030d7561765e83dd19110747c6a8e4816080e
DarkCodeUV <leonardo.m2349@gmail.com>
2021-12-23 05:22:30 -0600

    64_DOM_Traversing

---
+++ 64_DOM_JS/64_DOM_Traversing.js
@@ -0,0 +1,17 @@
+$cards = document.querySelector(".cards");
+
+console.log($cards);
+console.log($cards.children);//Acceder a los hijos (elementos)
+console.log($cards.children[2]); //Acceder a uno en particular
+console.log($cards.parentElement); //Acceder al padre de la etiqueta
+console.log($cards.firstChild); //Acceder al primer hijo  (Es de tipo text por el tabulador entre las etiquetas)
+console.log($cards.lastChild); //Acceder al ultimo hijo
+console.log($cards.previousElementSibling); //Acceder al hijo anterior
+console.log($cards.nextElementSibling); //Acceder al hijo siguiente
+
+/***
+ * Closest busca a al ancestro mas cercano (padre)
+ */
+console.log($cards.closest("div"));
+console.log($cards.closest("body"));
+console.log($cards.children[3].closest("secction"));
